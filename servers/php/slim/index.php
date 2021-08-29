5086178a9d3fbae9c0982651816afb0fffda7f80
ryyan <ryyan@pm.me>
2021-08-29 11:40:26 -0500

    Fix php/slim

--- servers/php/slim/index.php
+++ servers/php/slim/index.php
@@ -1,16 +1,31 @@
 <?php
+use \Psr\Http\Message\ServerRequestInterface as Request;
+use \Psr\Http\Message\ResponseInterface as Response;
+
+use Ramsey\Uuid\Uuid;
+use Ramsey\Uuid\Exception\UnsatisfiedDependencyException;
+use Slim\Factory\AppFactory;
 
 require __DIR__ . '/vendor/autoload.php';
 
-$settings = [
-	'settings' => [
-		'displayErrorDetails' => false,
-		'addContentLengthHeader' => false,
-		'outputBuffering' => false,
-	]
-];
-$app = new \Slim\App($settings);
 
-require __DIR__ . '/server.php';
+$app = AppFactory::create();
+
+$app->get('/api/echo/{words}', function (Request $request, Response $response) {
+  # Log request
+  # Automatically done by nginx
+
+  # Get parameters
+  $words = $request->getAttribute('words');
+  $num = $request->getQueryParam('num');
+
+  # Verify parameters
+  if (strtolower($words) != 'helloworld' || intval($num) != 1234567890) {
+    return $response->withJson(array('error' => 'Invalid parameters'), 400);
+  }
+
+  # Return response
+  return $response->withJson(array('message' => Uuid::uuid4()->toString()));
+});
 
 $app->run();
